23b046d316a01fde99a6df5a904180dd219df50e
Hyunje Alex Jun <me@noraesae.net>
2015-03-22 01:49:02 +0900

    Change common/bridge.js to use ES6.
    Yay!

--- common/bridge.js
+++ common/bridge.js
@@ -1,32 +1,34 @@
-// load ipc for both browser and renderer
-var ipc;
-try {
-  ipc = window.require('ipc');
-} catch(e) {
-  if (e.name === 'ReferenceError') {
-    ipc = require('ipc');
-  } else {
-    throw e;
-  }
+import _ipc from 'ipc';
+
+let ipc;
+if (typeof _require === 'function') {
+  // _require is renderer's native require
+  ipc = _require('ipc');
+} else {
+  ipc = _ipc;
 }
 
-module.exports = {
+export default {
   webContents: null,
-  initialize: function (browserWindow) {
+
+  initialize(browserWindow) {
     // only for a browser process
     this.webContents = browserWindow.webContents;
   },
-  sender: function () {
+
+  sender() {
     return this.webContents ? this.webContents : ipc;
   },
-  on: function (eventName, handler) {
+
+  on(eventName, handler) {
     ipc.on(eventName, function (argOrEvent, maybeArg) {
       // get arg from both browser and renderer
       var arg = typeof argOrEvent === 'string' ? argOrEvent : maybeArg;
       handler(JSON.parse(arg));
     });
   },
-  send: function (eventName, dataObj) {
+
+  send(eventName, dataObj) {
     this.sender().send(eventName, JSON.stringify(dataObj));
-  }
-};
+  },
+}
