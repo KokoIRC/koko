60539e3cba0146f33fcd1002406c068e8d036dcc
Hyunje Alex Jun <me@noraesae.net>
2015-03-23 14:08:14 +0900

    Implement 'logs' module.

--- renderer/lib/buffers.js
+++ renderer/lib/buffers.js
@@ -1,6 +1,8 @@
+import Logs from './logs';
+
 export default class Buffers {
   constructor(rootChannelName) {
-    this._buffers = [{name: rootChannelName, current: true}];
+    this._buffers = [{name: rootChannelName, current: true, logs: new Logs()}];
   }
 
   map(func) {
@@ -10,15 +12,14 @@ export default class Buffers {
   _bufferOps(target) {
     return {
       send: function (nick, text) {
-        // FIXME
-        console.log(target.name, nick, text);
+        target.logs.append(nick, text);
         return this;
       }.bind(this)
     }
   }
 
   to(name) {
-    var target = this._buffers.filter(c => (c.name === name))[0];
+    let target = this._buffers.filter(c => (c.name === name))[0];
     if (!target) {
       target = {
         name,
---
+++ renderer/lib/logs.js
@@ -0,0 +1,24 @@
+// FIXME: should be setting
+const scrollBackLimit = 1000;
+
+class Log {
+  constructor(nick, text) {
+    this.nick = nick;
+    this.text = text;
+    this.datetime = new Date();
+  }
+}
+
+export default class Logs {
+  constructor() {
+    this._logs = [];
+  }
+
+  append(nick, text) {
+    if (this._logs.length === scrollBackLimit) {
+      // pop the oldest log
+      this._logs.shift();
+    }
+    this._logs.push(new Log(nick, text));
+  }
+}
