1630504f3498db4bc7aa062d1000e5e6dbd5974f
Hyunje Alex Jun <me@noraesae.net>
2015-03-23 15:47:34 +0900

    Implement Buf.

--- renderer/lib/buffers.js
+++ renderer/lib/buffers.js
@@ -1,8 +1,26 @@
 import Logs from './logs';
 
+class Buf { // Buffer exists as a default class
+  constructor(name) {
+    this.name = name;
+    this.logs = new Logs();
+    this._current = false;
+  }
+
+  current(set) {
+    if (typeof set === 'undefined') {
+      return this._current;
+    } else {
+      this._current = set;
+    }
+  }
+}
+
 export default class Buffers {
-  constructor(rootChannelName) {
-    this._buffers = [{name: rootChannelName, current: true, logs: new Logs()}];
+  constructor(rootBufName) {
+    let rootBuf = new Buf(rootBufName);
+    rootBuf.current(true);
+    this._buffers = [rootBuf];
   }
 
   _bufferOps(target) {
@@ -17,18 +35,25 @@ export default class Buffers {
   to(name) {
     let target = this._buffers.filter(c => (c.name === name))[0];
     if (!target) {
-      target = {
-        name,
-        current: false,
-      };
+      target = new Buf(name);
       this._buffers.push(target);
     }
 
     return this._bufferOps(target);
   }
 
+  setCurrent(bufName) {
+    this._buffers = this._buffers.map(function (buffer) {
+      if (buffer.name === bufName) {
+        buffer.current(true);
+      } else {
+        buffer.current(false);
+      }
+    });
+  }
+
   current() {
-    return this._buffers.filter(c => c.current)[0];
+    return this._buffers.filter(c => c.current())[0];
   }
 
   map(func) {
