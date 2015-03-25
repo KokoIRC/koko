f00169a4862bb5ce6da4ff3b023200d8af0eebe8
Hyunje Alex Jun <me@noraesae.net>
2015-03-25 23:02:53 +0900

    Add ModeManager to handle mode change.

--- renderer/irc-window.js
+++ renderer/irc-window.js
@@ -4,12 +4,15 @@ import BufferView from './buffer-view';
 import InputBox from './input-box';
 import TabNav from './tab-nav';
 import React from 'react';
+import ModeManager, {Mode} from './lib/mode-manager';
 
 export default class IrcWindow extends React.Component {
   constructor(props) {
     super(props);
+    this.modeManager = new ModeManager(Mode.NORMAL);
     this.state = {
       buffers: new Buffers('~'),
+      mode: this.modeManager.current(),
     };
   }
 
@@ -19,6 +22,7 @@ export default class IrcWindow extends React.Component {
         buffers: this.state.buffers.to(data.to).send(data.nick, data.text)
       });
     }.bind(this));
+    this.initializeModeManger();
   }
 
   setWindowTitle(title) {
@@ -35,8 +39,45 @@ export default class IrcWindow extends React.Component {
       <div>
         <TabNav buffers={this.state.buffers} />
         <BufferView buffers={this.state.buffers} />
-        <InputBox />
+        <InputBox mode={this.state.mode} setMode={this.setMode.bind(this)} />
       </div>
     );
+  }
+
+  initializeModeManger() {
+    window.addEventListener('keydown', function (e) {
+      var currentMode = this.modeManager.current();
+      if (currentMode === Mode.NORMAL) {
+        if (e.which === 73 && !e.shiftKey) { // 'i'
+          this.setMode(Mode.INSERT);
+        } else if (e.which === 191 && !e.shiftKey) { // '/'
+          this.setMode(Mode.SEARCH);
+        } else if (e.which === 186 && e.shiftKey) { // ':'
+          this.setMode(Mode.COMMAND);
+        }
+      } else if (currentMode === Mode.INSERT) {
+        if (e.which === 27) { // 'esc'
+          this.setMode(Mode.NORMAL);
+        }
+      } else if (currentMode === Mode.SEARCH) {
+        if (e.which === 27) { // 'esc'
+          this.setMode(Mode.NORMAL);
+        } else if (e.which === 73 && !e.shiftKey) { // 'i'
+          this.setMode(Mode.INSERT);
+        }
+      } else if (currentMode === Mode.COMMAND) {
+        if (e.which === 27) { // 'esc'
+          this.setMode(Mode.NORMAL);
+        }
+      }
+    }.bind(this));
+
+    this.modeManager.onChange(function (to) {
+      this.setState({mode: to});
+    }.bind(this));
+  }
+
+  setMode(mode) {
+    this.modeManager.setMode(mode);
   }
 }
---
+++ renderer/lib/mode-manager.js
@@ -0,0 +1,28 @@
+export const Mode = {
+  NORMAL: 0,
+  INSERT: 1,
+  SEARCH: 2,
+  COMMAND: 3,
+};
+
+export default class ModeManager {
+  constructor(initialMode) {
+    this._mode = initialMode;
+    this._onChange = null;
+  }
+
+  current() {
+    return this._mode;
+  }
+
+  setMode(mode) {
+    let previous = this._mode;
+    this._mode = mode;
+    console.log(previous, mode);
+    setTimeout(this._onChange.bind(null, mode, previous), 0);
+  }
+
+  onChange(callback) {
+    this._onChange = callback;
+  }
+}
