1a350ed3beaaa14a962a4742b629981afa292fa5
Hyunje Alex Jun <me@noraesae.net>
2015-05-19 14:06:27 +0900

    Add a way to handle custom configuration.
    A config file saved in `~/.koko.yml` will be parsed as user
    configuration.

--- browser/configuration.ts
+++ browser/configuration.ts
@@ -4,15 +4,36 @@ import path = require('path');
 import yaml = require('js-yaml');
 
 const configDir = path.join(__dirname, '../config');
+const userConfigPath = path.join(process.env.HOME || process.env.USERPROFILE,
+                                 '.koko.yml');
+
+function customize(configs: any, userConfigs: any) {
+  if (configs.constructor === Array) {
+    return configs.concat(userConfigs);
+  } else if (configs.constructor === Object) {
+    if (userConfigs.constructor === Object) {
+      Object.keys(userConfigs).forEach(key => {
+        if (configs[key]) {
+          configs[key] = customize(configs[key], userConfigs[key]);
+        } else {
+          configs[key] = userConfigs[key];
+        }
+      });
+    }
+    return configs;
+  } else {
+    return userConfigs;
+  }
+}
 
 export function load(): any {
-  let configs = fs.readdirSync(configDir);
-  configs = configs
+  let configs = fs.readdirSync(configDir)
     .filter(c => c.endsWith('.yml'))
     .reduce((result: any, c) => {
       let ymlContent = fs.readFileSync(path.join(configDir, c)).toString();
       result[path.basename(c, path.extname(c))] = yaml.load(ymlContent);
       return result;
     }, {});
-  return configs;
+  let userConfigs = yaml.load(fs.readFileSync(userConfigPath).toString());
+  return customize(configs, userConfigs);
 }
